1dd137c0ce010162833f0d4ba20218f79a875780
luis <luisfernandoromeroramos85@gmail.com>
2024-06-12 01:32:35 +0530

    Cards component created

---
+++ client/src/components/Cards.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Cards = ({item}) => {
+    console.log(item.image);
+
+  return (
+    <>
+      <div className="mt-6 my-3 ">
+        <div className="card w-96 bg-base-100 shadow-xl hover:shadow-xl hover:bg-green-400 hover:text-white hover:duration-300">
+          <figure>
+            <img src={item.image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions justify-between">
+              <div className="badge badge-outline">${item.price}</div>
+              <div className="badge badge-outline cursor-pointer hover:bg-blue-500 hover:border-blue-700 hover:text-white duration-300">Buy Now</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cards;
