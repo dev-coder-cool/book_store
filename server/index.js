1f72b8879d4888ab5843857d380345c39c5294e2
luis <luisfernandoromeroramos85@gmail.com>
2024-06-19 21:54:24 +0530

    MongoDB Connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express()
 dotenv.config();
-const PORT = process.env.PORT || 4000 
+const PORT = process.env.PORT || 4000
+const URI = process.env.MongoDBURI;
 
-app.get('/', (req, res) => {
-  res.send('Hello World...!');
-});
+//MongoDB connection
+try {
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log('MongoDB Connection Success');
+} catch (error) {
+  console.log("Error: ", error);
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is Started Port: ${PORT}`);
