41ba51ea20e1b1f42a392ad72f8491188a579a92
luis <luisfernandoromeroramos85@gmail.com>
2024-06-20 13:39:04 +0530

    Route  created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
+import bookRoute from "./route/book.route.js"
+
 const app = express()
 dotenv.config();
 const PORT = process.env.PORT || 4000
@@ -17,6 +19,11 @@ try {
 } catch (error) {
   console.log("Error: ", error);
 }
+
+
+// defining routes
+app.use("/book",bookRoute)
+
 
 
 app.listen(PORT, () => {
